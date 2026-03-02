30961397a9b984cfce59d084445f31f03b16c3fb
adenlall <adenlall@proton.me>
2026-03-02 14:18:30 -0500

    Update fish scripts

--- dots/.config/fish/functions/dots.fish
+++ dots/.config/fish/functions/dots.fish
@@ -1,25 +1,25 @@
 function dots --wraps='cd ~/.config' --description 'Manage the dots on ~/.dotsys'
     switch "$argv[1]"
         case cd
-            cd /path/to/your/specific/folder
+            cd ~/.dotsys
         case pull
             switch "$argv[2]"
                 case fast
                     echo "pulling with --skip-alldeps --skip-allgreeting"
-                    ./setup install --skip-alldeps --skip-allgreeting
+                    ~/.dotsys/setup install --skip-alldeps --skip-allgreeting
                 case files
                     echo "pulling with --skip-alldeps"
-                    ./setup install --skip-alldeps
+                    ~/.dotsys/setup install --skip-alldeps
                 case deps
                     echo "pulling with"
-                    ./setup install
+                    ~/.dotsys/setup install
                 case kill
                     echo "killing qs and pulling with fast"
                     kill qs
-                    ./setup install --skip-alldeps --skip-allgreeting || qs -c ii
+                    ~/.dotsys/setup install --skip-alldeps --skip-allgreeting || qs -c ii
                 case ''
                     echo "pulling with --skip-alldeps --skip-allgreeting"
-                    ./setup install --skip-alldeps --skip-allgreeting
+                    ~/.dotsys/setup install --skip-alldeps --skip-allgreeting
                 case '*'
                     echo "dots.fish pull [subcommand]: uknown $argv[2]"
                     return 1
