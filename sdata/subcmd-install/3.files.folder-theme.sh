c104f6fb1bad371fe121d5d3ca1c1263ec4b8528
adenlall <adenlall@proton.me>
2026-03-20 17:45:41 -0400

    Auto select WhiteSur Icon theme when user enable folder theming

--- sdata/subcmd-install/3.files.folder-theme.sh
+++ sdata/subcmd-install/3.files.folder-theme.sh
@@ -1,6 +1,6 @@
 # Polkit settup
 
-ICON_PACK="WhitseSurs"
+ICON_PACK="WhiteSur"
 ICON_DIR="$XDG_DATA_HOME/icons/$ICON_PACK"
 ICON_GIT="https://github.com/vinceliuice/WhiteSur-icon-theme"
 INSTALL_DIR="/tmp/$ICON_PACK-install-end4"
@@ -41,7 +41,6 @@ if [ -d "$INSTALL_DIR" ]; then
 fi
 
 git clone $ICON_GIT $INSTALL_DIR
-bash /tmp/$ICON_PACK-install/install.sh -b
 
 
 if [ ! -d "$ICON_DIR" ]; then
@@ -60,9 +59,21 @@ if [ ! -d "$ICON_DIR" ]; then
 fi
 
 
+bash $INSTALL_DIR/install.sh -b
+sed -i "s/^iconslight = .*/iconslight = $ICON_PACK/" "$XDG_CONFIG_HOME/kde-material-you-colors/config.conf"
+sed -i "s/^iconsdark = .*/iconsdark = $ICON_PACK/" "$XDG_CONFIG_HOME/kde-material-you-colors/config.conf"
+sed -i "/^\[Icons\]/,/^\[/ s/^Theme=.*/Theme=$ICON_PACK/" "$XDG_CONFIG_HOME/kdeglobals"
+
 
 printf "\n${STY_GREEN}${STY_BOLD}"
 echo "-----------------------------------"
 printf "$ICON_PACK installed successfuly!\n"
 echo "-----------------------------------"
 printf "\n${STY_GREEN}${STY_RST}"
+
+
+printf "\n${STY_YELLOW}${STY_BOLD}"
+echo "-----------------------------------"
+printf "You need to Reload the shell to apply the Icon theme globaly! [Super+Ctrl+R]\n"
+echo "-----------------------------------"
+printf "\n${STY_YELLOW}${STY_RST}"
